a80d3b15241282e727a7cedd7a21a03d99e78a53
Jakub <meerkat.stack@gmail.com>
2026-01-07 00:03:21 +0100

    Add login buttons and functional text cursor.

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ int main()
     3 - Guess
     4 - Game menu
     */
+   int active_field = -1;//Zmienna określająca które pole ma przyjmować tekst, tam ustawia kursor
+   unsigned int frame_count=0;//Zlicza klatki, przyda się do kursora
 
 
     //Torzy okno
@@ -56,6 +58,9 @@ int main()
     sf::Text labels_log[2]={sf::Text(font),sf::Text(font)};//Przechowuje napisy przy polach do logowania
     sf::RectangleShape login_button({0.12f*width,0.05f*height});//Przycisk logowania
     sf::Text login_button_label(font, "Log in", 0.035f*height);//Etykieta przycisku logowania
+    sf::RectangleShape back_button({0.12f*width,0.05f*height});//Przycisk logowania
+    sf::Text back_button_label(font, "Back", 0.035f*height);//Etykieta przycisku logowania
+
 
 
     //Pola i ramki
@@ -68,22 +73,31 @@ int main()
     labels_log[0] = label((int)(0.5f*0.037f*height+3),(int)( width/2.0f-0.1f*width),(int)(height/2.0f-0.06f*height-0.029f*height), "Login",Dark_color,font);
     labels_log[1] = label((int)(0.5f*0.037f*height+3), (int)(width/2.0f-0.1f*width+10),(int)(height/2.0f+0.06f*height-0.029f*height), "Password",Dark_color,font);
 
-    //Przycisk
-    login_button = build_button(0.12f*width,0.05f*height,width/2.0f,height/2.0f+0.155f*height,Apatite_color,Dark_color);
+    //Przyciski
+    login_button = build_button(0.12f*width,0.05f*height,width/2.0f+44,height/2.0f+0.155f*height,Apatite_color,Dark_color);
+    login_button_label = label(0.035f*height, width/2.0f+44,height/2.0f+0.155f*height, "Log in",Dark_color,font);
 
-    login_button_label = label(0.035f*height, width/2.0f,height/2.0f+0.155f*height, "Log in",Dark_color,font);
+    back_button = build_button(0.12f*width,0.05f*height,width/2.0f-55,height/2.0f+0.155f*height,Apatite_color,Dark_color);
+    back_button_label = label(0.035f*height, width/2.0f-55,height/2.0f+0.155f*height, "Back",Dark_color,font);
 
-
-
+    //Kursor
+    sf::RectangleShape cursor({2.0f,29.0f});
+    cursor.setFillColor(sf::Color(Dark_color));
+    cursor.setOrigin(cursor.getGeometricCenter());
 
 
 
 
     //Pętla gry, wykonuje się do puki do puty okno jest otwarte
     while(window->isOpen()){
-        
+        {
+        if(frame_count> 60) frame_count = 0;
+        else frame_count++;
+        }
+
         //Czyta zdażenia, takie jak przyciski,ruchy myszką itd., otem wykonuje odpowiednie akcje
-        while(const std::optional event = window->pollEvent()){
+        //Pętla zdażeń
+        while (const std::optional<sf::Event> event = window->pollEvent()){
 
             //Jak wcisnąć krzyzyk to sie zamyka
             if(event->is<sf::Event::Closed>()){
@@ -98,7 +112,7 @@ int main()
                     window->close();
                 }
             }
-            //Sprawdza kliknięcie guzików
+            //Sprawdza kliknięcie guzików na starcie
             else if(state==0){
                 for(int i=0;i<max_num_of_buttons;i++){
                     if(button_action(buttons[i],*event,*window)){
@@ -110,8 +124,18 @@ int main()
             }
             else if(state==1){
                 if(button_action(login_button,*event,*window)){
-                    std::cout<<"LOG ON!!!" << std::endl;
+                    std::cout<<"LOG IN!!!   " << std::endl;
+                    std::cout<<active_field;
                 }
+                if(button_action(back_button,*event,*window)){
+                    state = 0;//Powraca do menu startowego
+                    active_field = -1;
+                }
+                {
+                if(button_action(text_frames[2],*event,*window)) active_field = 0;//Ustawia pole loginu na aktywne
+                else if(button_action(text_frames[3],*event,*window)) active_field = 1;//Ustawia pole hasła na aktywne
+                }
+
             }
         }
         //Animacja guzików
@@ -122,6 +146,7 @@ int main()
         }
         else if(state==1){
             button_animation(login_button,login_button_label,*window);
+            button_animation(back_button,back_button_label,*window);
         }
         
 
@@ -138,6 +163,19 @@ int main()
             for(int i=0;i<2;i++){window->draw(labels_log[i]);}//Wyświetla opisy pól
             window->draw(login_button);
             window->draw(login_button_label);
+            window->draw(back_button);
+            window->draw(back_button_label);
+            if(active_field!=-1){
+                if(active_field==0){
+                    cursor.setPosition({width/2.0f-80,height/2.0f-0.06f*height});
+                    if(frame_count>30) window->draw(cursor);
+                }
+                if(active_field==1){
+                    cursor.setPosition({width/2.0f-80,height/2.0f+0.06f*height});
+                    if(frame_count>30) window->draw(cursor);
+                }
+
+            }
 
             break;
         case 2:

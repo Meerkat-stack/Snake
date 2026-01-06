2a37206083ba07dc3c30f3357c36bf664af6adc1
Jakub <meerkat.stack@gmail.com>
2026-01-07 00:03:25 +0100

    Add comments.

--- includes/user.cpp
+++ includes/user.cpp
@@ -26,20 +26,4 @@ sf::RectangleShape text_frame_bg(float a, float b, float x, float y, unsigned in
 }
 
 
-sf::Text text_label(int size, float x, float y, std::string label_text,unsigned int color,sf::Font& font){
-    sf::Text text(font, label_text, size);//Ustawia dla napisu, czcionkę, tekst, rozmiar
-    text.setFillColor(sf::Color(color));
-    // text.setOutlineColor(sf::Color(Dark_color));
-    // text.setOutlineThickness(-1.f);
 
-    //Ustawia punkt odniesienia napisu 
-    sf::FloatRect bounds = text.getLocalBounds();
-    // text.setOrigin({bounds.position.x, 0.f});//Wyrównuje do lewej
-    // text.setOrigin({bounds.position.x+bounds.size.x, 0.f});//Wyrównuje do prawej
-    text.setOrigin({bounds.position.x+bounds.size.x/2,//Wyrównuje do środka
-                    (float)text.getCharacterSize()*0.6f});//To ustawia dolny punkt odniesienia na linijkę do pisania
-                    //Tak samo jak masz linijki w zeszycie to tu the same
-    text.setPosition({x,y});
-
-    return text;
-}
